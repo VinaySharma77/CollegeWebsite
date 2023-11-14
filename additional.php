906e5a6af33c0100b96a844c6134d509f0435032
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-14 17:09:22 +0530

    Add files via upload

---
+++ additional.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Graduate Government College | PGGC11</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/logo.jpg">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="additional">
+            <div class="helpline">
+                <p>Helpline No. <i class="fa-solid fa-phone"></i> 0172-2740597, 77101-14580</p>
+            </div>
+            <div class="zoom-area">
+                <span class="site-map" title="site-map"><i class="fa-solid fa-sitemap"></i></span>
+                <span class="zoom-out" id="zoom-out" title="Zoom-Out">A-</span>
+                <span class="normal-font" id="normal-font" onclick="normal()" title="Normal Font">A</span>
+                <span class="zoom-in" id="zoom-in" onclick="zoom()" title="Zoom-In">A+</span>
+            </div>
+        </div>
+        <div class="header">
+            <div class="logo-area">
+                <div class="logo">
+                    <img src="img/logo.jpg" alt="PGGC11 Logo">
+                </div>
+                <div class="college-name">
+                    <h2>Post Graduate Government College</h2>
+                    <p>Sector 11, Chandigarh</p>
+                </div>
+            </div>
+            <div class="g20">
+                <img src="img/g20.jpg" alt="G20 Logo">
+            </div>
+            <div class="login-area">
+                <a id="staff" href="login/staff-login.php">Staff Login</a>
+                <a id="web" href="login/web-login.php">Web Login</a>
+            </div>
+        </div>
+        <div class="navbar">
+            <div class="nav-links" id="nav-links">
+                <div class="mobile-navbar-btn">
+                    <i class="fa-solid fa-bars" id="menu"></i>
+                    <i class="fa-solid fa-xmark" id="close-menu"></i>
+                </div>
+                <a href="additional.php">HOME</a>
+                <a href="additional.php">INSTITUTE</a>
+                <a href="additional.php">ADMINISTRATION</a>
+                <a href="additional.php">LIBRARY</a>
+                <a href="additional.php">DEPARTMENT</a>
+                <a href="additional.php">ADMISSIONS</a>
+                <a href="additional.php">PROSPECTUS</a>
+                <a href="additional.php">E-LEARNING</a>
+                <a href="additional.php">CONTACT US</a>
+            </div>
+        </div>
+        <div class="main">
+            <h2>Information About PGGC11</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut libero quia dicta dignissimos excepturi id amet similique quibusdam dolorum pariatur dolore quam iure sed iusto consequuntur modi quis reiciendis, laudantium labore. Quasi porro accusantium harum doloribus quos voluptatum earum inventore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti quidem doloremque dolorem neque voluptas dicta cumque provident, rerum accusantium amet deleniti, quis quos cum dolor harum voluptatibus veritatis! Sed iure voluptatum numquam soluta quibusdam, minus eligendi magni ex magnam laboriosam aliquam est illum nostrum unde exercitationem, iste hic voluptates consequatur, expedita eum fugit saepe dolores velit! Eum officia velit ab, earum at dignissimos pariatur beatae ipsum tempore, iusto provident repudiandae quos laudantium culpa porro recusandae sed quisquam, a omnis. Sunt consequuntur est delectus incidunt ad unde quae necessitatibus excepturi facere temporibus fugiat, a quos odio autem harum repudiandae aperiam. Autem consequuntur, quod laudantium, nostrum porro quo cum optio in et, voluptas laboriosam officiis nihil nisi enim fugiat dolorum eos esse ratione perspiciatis quos deleniti? Debitis quae laborum earum, rem ad velit aspernatur repellat fugit doloribus dolorum similique sit asperiores placeat voluptatem id maiores aliquam ipsum? Cumque, itaque ad molestias magnam rerum corporis dolores? Maxime odio possimus nobis pariatur ex, dolorum aliquam velit officia aut dignissimos animi, eaque nihil voluptatibus assumenda? Impedit, quidem quisquam ea molestias inventore, error nulla quo iusto repellendus at quasi vero dolore in porro corporis doloremque illo placeat, eos eaque. Laudantium laborum ad expedita voluptate veniam aliquid! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, dolorum quos dolorem explicabo asperiores sit quisquam excepturi, praesentium at laboriosam assumenda. Dolore numquam magnam earum, reiciendis, minus rerum eius fugit pariatur reprehenderit eligendi quaerat in corrupti minima officiis, vero hic tempora. Obcaecati ea at expedita tempore temporibus delectus corrupti inventore, consectetur error corporis! Mollitia laudantium est et, rerum accusamus tenetur vero. Nesciunt sint cumque in sunt aspernatur consequuntur mollitia natus accusamus facere libero earum nisi sapiente temporibus maiores esse porro impedit vero ratione, voluptas excepturi. Beatae porro libero blanditiis ex deserunt, ab, eos doloremque, sapiente deleniti aliquam facere voluptas maiores nesciunt. Expedita molestias totam placeat tempora alias deserunt. Iste at amet architecto reiciendis magnam ab temporibus excepturi illum ipsa nihil tempore consectetur odit, molestiae perspiciatis non porro! Numquam, alias beatae magnam, dignissimos nulla ullam molestiae delectus iste voluptas pariatur perspiciatis rem voluptatum. Ea totam ad sed quaerat temporibus, inventore vero beatae tenetur voluptates eaque neque tempore quam unde placeat consequatur expedita, obcaecati, qui porro recusandae iusto eius saepe veniam quos. Nostrum sed ut, accusantium repudiandae provident unde iure deserunt deleniti eos blanditiis eaque eius amet. Modi delectus eum hic earum veniam! Eos dolorem animi enim aspernatur possimus! Labore, eveniet ipsa.</p>
+        </div>
+        <div class="add-footer">
+            <div class="ft-box">
+                <h2>External Links</h2>
+                <a href="#">Chandigarh Administration</a>
+                <a href="#">University Grants Commission</a>
+                <a href="#">Panjab University Chandigarh</a>
+                <a href="#">Ministry of Human Resource Development</a>
+                <a href="#">Rashtriya Uchchatar Shiksha Abhiyan</a>
+                <a href="#">National Council for Teacher Education</a>
+            </div>
+            <div class="ft-box">
+                <h2>Infrastructure Facilities</h2>
+                <a href="#">Campus Infrastructure</a>
+                <a href="#">Campus Facilities</a>
+                <a href="#">Sports Facilities</a>
+                <a href="#">Hostel Facilities</a>
+                <a href="#">Physically Challenged Person Facilities</a>
+                <a href="#">Affidavit for seeking recognition of M.Ped</a>
+            </div>
+            <div class="ft-box">
+                <h2>Other Links</h2>
+                <a href="#">Achievements</a>
+                <a href="#">Gallery</a>
+                <a href="#">Downloads</a>
+                <a href="#">RTI</a>
+                <a href="#">Cyber Crime Awareness</a>
+            </div>
+            <div class="ft-box">
+                <h2>Get In Touch</h2>
+                <p>Post Graduate Government College,<br>
+                Sector - 11, Madhya Marg,<br>
+                Chandigarh, 160011<br>
+                Phone No:+91-172–2740597<br>
+                EMAIL ID : principal@gc11.ac.in<br>
+                WEBSITE : www.gc11.ac.in</p>
+                <div class="social-icons">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            Copyright © Post Graduate Government College, Sector 11, Chandigarh, India | Designed by Vinay Sharma
+        </div>
+    </div>
+    <script src="js/zoom-in.js"></script>
+    <script src="js/normal.js"></script>
+    <script src="js/zoom-out.js"></script>
+    <script src="js/menu.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Graduate Government College | PGGC11</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/logo.jpg">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="overlay" id="overlay"></div>
+    <div class="pop-up" id="pop-up" onload="pop_up()">
+        <button class="close" id="close" title="close">&times;</button>
+        <img src="img/logo.jpg" alt="PGGC11 Logo">
+        <h2>Welcome to Website of Post Graduate Government College Sector 11, Chandigarh</h2>
+        <p>Admission Helpline No. <i class="fa-solid fa-phone"></i> 0172-2740597, 77101-14580</p>
+    </div>
+    <div class="container">
+        <div class="additional">
+            <div class="helpline">
+                <p>Helpline No. <i class="fa-solid fa-phone"></i> 0172-2740597, 77101-14580</p>
+            </div>
+            <div class="zoom-area">
+                <span class="site-map" title="site-map"><i class="fa-solid fa-sitemap"></i></span>
+                <span class="zoom-out" id="zoom-out" title="Zoom-Out">A-</span>
+                <span class="normal-font" id="normal-font" onclick="normal()" title="Normal Font">A</span>
+                <span class="zoom-in" id="zoom-in" onclick="zoom()" title="Zoom-In">A+</span>
+            </div>
+        </div>
+        <div class="header">
+            <div class="logo-area">
+                <div class="logo">
+                    <img src="img/logo.jpg" alt="PGGC11 Logo">
+                </div>
+                <div class="college-name">
+                    <h2>Post Graduate Government College</h2>
+                    <p>Sector 11, Chandigarh</p>
+                </div>
+            </div>
+            <div class="g20">
+                <img src="img/g20.jpg" alt="G20 Logo">
+            </div>
+            <div class="login-area">
+                <a id="staff" href="login/staff-login.php">Staff Login</a>
+                <a id="web" href="login/web-login.php">Web Login</a>
+            </div>
+        </div>
+        <div class="navbar">
+            <div class="nav-links" id="nav-links">
+                <div class="mobile-navbar-btn">
+                    <i class="fa-solid fa-bars" id="menu"></i>
+                    <i class="fa-solid fa-xmark" id="close-menu"></i>
+                </div>
+                <a href="additional.php">HOME</a>
+                <a href="additional.php">INSTITUTE</a>
+                <a href="additional.php">ADMINISTRATION</a>
+                <a href="additional.php">LIBRARY</a>
+                <a href="additional.php">DEPARTMENT</a>
+                <a href="additional.php">ADMISSIONS</a>
+                <a href="additional.php">PROSPECTUS</a>
+                <a href="additional.php">E-LEARNING</a>
+                <a href="additional.php">CONTACT US</a>
+            </div>
+        </div>
+        <div class="main-bg"></div>
+        <div class="news-box">
+                <div class="news">
+                    <div class="college-announcements">
+                        <p>College Announcements</p>
+                    </div>
+                    <div class="news-links">
+                        <div class="marquee">
+                            <a href="additional.php">NSS registration is open for all streams and year (2023-24). The last date for registration is 25/10/2023.<span class="new">New</span></a>
+                            <a href="additional.php">Application Form for Walk-in Interview for temporary faculty in Self –Financed Courses</a>
+                            <a href="additional.php">PGGC11: Notice for students<span class="new">New</span></a>
+                            <a href="additional.php">PGGC11 3rd Counselling for Boys Hostel Admission Session 2023-24</a>
+                            <a href="additional.php">Application Form for Walk-in Interview for temporary faculty in Self –Financed Courses</a>
+                            <a href="additional.php">PGGC11: Notice for students</a>
+                            <a href="additional.php">PGGC11 3rd Counselling for Boys Hostel Admission Session 2023-24<span class="new">New</span></a>
+                            <a href="additional.php">NSS registration is open for all streams and year (2023-24). The last date for registration is 25/10/2023.<span class="new">New</span></a>
+                            <a href="additional.php">Application Form for Walk-in Interview for temporary faculty in Self –Financed Courses</a>
+                            <a href="additional.php">PGGC11: Notice for students<span class="new">New</span></a>
+                            <a href="additional.php">PGGC11 3rd Counselling for Boys Hostel Admission Session 2023-24</a>
+                            <a href="additional.php">Application Form for Walk-in Interview for temporary faculty in Self –Financed Courses</a>
+                        </marquee>
+                    </div>
+                </div>
+            </div>
+        <div class="principal">
+            <h2>Principal's Message</h2>
+            <div class="principal-box">
+                <div class="principal-message">
+                    <p>Post Graduate Govt. College Sector 11, now a Co-educational Institute, was established in 1953. Over the years, our illustrious students have carved a niche for themselves in fields as diverse as Politics, Administration, Business, Law etc. We offer a broad curriculum and a range of opportunities for all our students to achieve excellence in academic, creative, social, cultural, sporting and community endeavours.
+                        There are as many as 30 departments in Humanities, Commerce, Science, Information Technology, Physical Education as well as PG courses in Chemistry, Commerce, History, Hindi, English, Physics, Punjabi, Physical Education and Information Technology.
+                        The college boasts of a large sports ground with cinder track and a sports pavilion. There is also a swimming pool, a gymnasium, tennis, golf course and a basketball court. It has a department of Environment Education with a highly specialized Cosmic Garden, the first of its kind in India with special reference to Phytochemicals as an alternative medicine.
+                        Our college is spread over an area of 43.8 acres and is equipped with the latest technology, numerous internet labs, Wi-Fi facility, overall e-surveillance and the largest library among the college in this city. The library is fully computerized with online access and a new feature of transaction messaging through SMS. It has made old question papers and data of pending degrees since 1958 available online. It has a separate Braille corner for visually challenged students.
+                        Students, we believe are our most valued assets. A dedicated team of our teachers monitor and mentor young students to help them imbibe attributes, skills and capabilities to achieve their cherished goals. I am positive that in times to come, we will make steady progress in this rewarding journey of teaching and learning in Post Graduate Govt. College Sector 11, Chandigarh.</p>
+                </div>
+                <div class="principal-photo">
+                    <div class="read-more">
+                    <a href="additional.php"><img src="img/principal.jpeg" alt="Principal Photo" id="principal"></a>
+                    </div>
+                    <h3>Prof (Dr.) Punam Agarwal</h2>
+                    <p>Principal</p>
+                </div>
+            </div>
+        </div>
+            <div class="slidebox">
+                <div class="slide-head">College Latest Events</div>
+                <div class="carousel-container">
+                    <div class="carousel-slide">
+                        <img src="img/slide1.jpeg" alt="Image 1">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide2.jpeg" alt="Image 2">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide3.jpeg" alt="Image 3">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide4.jpeg" alt="Image 4">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide5.jpeg" alt="Image 5">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide6.jpg" alt="Image 6">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide7.jpeg" alt="Image 7">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide8.jpeg" alt="Image 8">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide9.jpeg" alt="Image 9">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide10.jpeg" alt="Image 10">
+                    </div>
+                    <button class="prev-button">&#10094;</button>
+                    <button class="next-button">&#10095;</button>
+                </div>
+            </div>
+        </div>
+        <h2 class="gallery">Our Gallery</h2>
+        <div class="cards">
+            <div class="card card1">
+                <div class="content">
+                    <h1>Facility 1</h1>
+                    <p>Nature is beautiful</p>
+                </div>
+            </div>
+            <div class="card card2">
+                <div class="content">
+                    <h1>Facility 2</h1>
+                    <p>Nature is beautiful</p>
+                </div>
+            </div>
+            <div class="card card3">
+                <div class="content">
+                    <h1>Facility 3</h1>
+                    <p>Nature is beautiful</p>
+                </div>
+            </div>
+            <div class="card card4">
+                <div class="content">
+                    <h1>Facility 4</h1>
+                    <p>Nature is beautiful</p>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="ft-box">
+                <h2>External Links</h2>
+                <a href="additional.php">Chandigarh Administration</a>
+                <a href="additional.php">University Grants Commission</a>
+                <a href="additional.php">Panjab University Chandigarh</a>
+                <a href="additional.php">Ministry of Human Resource Development</a>
+                <a href="additional.php">Rashtriya Uchchatar Shiksha Abhiyan</a>
+                <a href="additional.php">National Council for Teacher Education</a>
+            </div>
+            <div class="ft-box">
+                <h2>Infrastructure Facilities</h2>
+                <a href="additional.php">Campus Infrastructure</a>
+                <a href="additional.php">Campus Facilities</a>
+                <a href="additional.php">Sports Facilities</a>
+                <a href="additional.php">Hostel Facilities</a>
+                <a href="additional.php">Physically Challenged Person Facilities</a>
+                <a href="additional.php">Affidavit for seeking recognition of M.Ped</a>
+            </div>
+            <div class="ft-box">
+                <h2>Other Links</h2>
+                <a href="additional.php">Achievements</a>
+                <a href="additional.php">Gallery</a>
+                <a href="additional.php">Downloads</a>
+                <a href="additional.php">RTI</a>
+                <a href="additional.php">Cyber Crime Awareness</a>
+            </div>
+            <div class="ft-box">
+                <h2>Get In Touch</h2>
+                <p>Post Graduate Government College,<br>
+                Sector - 11, Madhya Marg,<br>
+                Chandigarh, 160011<br>
+                Phone No:+91-172–2740597<br>
+                EMAIL ID : principal@gc11.ac.in<br>
+                WEBSITE : www.gc11.ac.in</p>
+                <div class="social-icons">
+                <a href="additional.php"><i class="fa-brands fa-facebook"></i></a>
+                <a href="additional.php"><i class="fa-brands fa-twitter"></i></a>
+                <a href="additional.php"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href="additional.php"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            Copyright © Post Graduate Government College, Sector 11, Chandigarh, India | Designed by Vinay Sharma
+        </div>
+    </div>
+    <script src="js/zoom-in.js"></script>
+    <script src="js/normal.js"></script>
+    <script src="js/zoom-out.js"></script>
+    <script src="js/pop-up.js"></script>
+    <script src="js/carousel.js"></script>
+    <script src="js/menu.js"></script>
+</body>
+</html>
