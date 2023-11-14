8127922b8de0e3ba09e583dc599f5338dd618856
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-14 17:13:14 +0530

    Add files via upload

---
+++ login/web-login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Login | PGGC11</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="img/logo.jpg">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap" rel="stylesheet">
+</head>
+<body class="body-bg">
+    <div class="box">
+        <h2>Web Login</h2>
+        <div class="staff-login">
+            <form action="" id="web-form" method="post">
+                <img src="../img/logo.jpg" alt="PGGC11 Logo">
+                <input type="text" name="uname" id="uname" placeholder="Username..." autocomplete="off">
+                <input type="password" name="pass" id="pass" placeholder="Password..." autocomplete="off">
+                <a href="register.php">Create New Account</a>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
